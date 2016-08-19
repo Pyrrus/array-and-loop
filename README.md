ef3663d082381508266ef7212ecd1cdb41ebbd6d
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-08-19 11:35:38 -0700

    Add readme.md

---
+++ README.md
@@ -0,0 +1,38 @@
+# Ping-pong site
+
+## Ping-pong using Javascript & Jquery, 8/19/16
+
+#### By **Adam Gorbahn**
+
+## Description
+
+This page uses input to find what number to get the a ping, a pong or both.
+
+## User interface & business logic
+
+User input    | Output (Business login)
+------------- | -------------
+6             | [1, 2, ping, 4, pong]
+20            | [1, 2, ping, 4, pong, 6, 7, 8, ping, pong, 11, ping, 13, 14, pingpong, 16, 17, ping, 19, pong]
+
+## Setup/Installation Requirements
+
+* Just need a web browser
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* Jquery
+
+## Link
+
+http://pyrrus.github.io/array-and-loop
+
+### License
+
+*GPL*
+
+Copyright (c) 2016 **Adam Gorbahn**
--- js/script.js
+++ js/script.js
@@ -1,45 +1,66 @@
+// set the global var to help with 
+// the timeing of the css animation.
 var time = 0;
 
+// add form validation to test the it 
+// have number or not
 var validation = function(number) {
   var pattern = /^\d+$/;
   return pattern.test(number);
 }
 
+// add the classes for the ball 
+// go to right
 var ballRight = function () {
+  // set time out to give the ball 
+  // time to go to the other side of
+  // the screen
    setTimeout(function() { 
     $("#ball").removeClass("leftM")
     $("#ball").addClass("rightM")
   }, time += 3000);
 
+   // remove the right class and 
+   // add left class
    setTimeout(function (argument) {
      $("#ball").removeClass("right");
      $("#ball").addClass("left");
    }, time += 3000);
 }
 
+// add the classes for the ball 
+// go to left
 var ballLeft = function () {
+    // set time out to give the ball 
+    // time to go to the other side of
+    // the screen
    setTimeout(function() { 
     $("#ball").removeClass("rightM")
     $("#ball").addClass("leftM")
   }, time += 3000);
 
+   // remove the left class and 
+   // add right class
    setTimeout(function (argument) {
      $("#ball").removeClass("left");
      $("#ball").addClass("right")
    }, time += 3000);
 }
 
-function doSetTimeout(i) {
-  setTimeout(function() { alert(i); }, 100);
-}
-
+// set the word for ping and pong 
+// reset the ball and time.
 var pingPong = function(number) {
   var holder = [];
+
+  // reset the ball
   $("#ball").removeClass("left");
   $("#ball").removeClass("leftM");
   $("#ball").removeClass("rightM");
   $("#ball").addClass("right");
   time = 0;
+  // end ball
+
+  // set the ping and pong to the array
   for (var i = 1; i <= number; i++) {
     if ((i % 3 === 0) && (i % 5 === 0)) {
       holder.push("pingpong");
@@ -57,6 +78,8 @@ var pingPong = function(number) {
   return holder;
 }
 
+// This display the information 
+// for the array
 var display = function(data) {
   $(".remove").remove();
   for (var i = 0; i < data.length; i++) {
@@ -67,6 +90,7 @@ var display = function(data) {
 // Jquery ready
 $(document).ready(function() {
 
+  // user input
   $("button").click(function(event) {
     var number = $("#num");
     var data = [];
