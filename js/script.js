c902ef58df8e8edc1829bd130fffa36c1c8c5242
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-08-19 08:54:20 -0700

    Add ping pong funtion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
+var validation = function(number) {
+    var pattern = /^\d+$/;
+    return pattern.test(number);
+}
+
+var pingPong = function (number) {
+  var holder = [];
+  for (var i = 1; i <= number; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      holder.push("pingpong")
+    } else if (i % 3 === 0) {
+      holder.push("ping")
+    } else if (i % 5 === 0) {
+      holder.push("pong")
+    } else {
+      holder.push(i);
+    }
+  }
+
+  return holder;
+}
+
 // Jquery ready
 $(document).ready(function() {
 
+  $("button").click(function (event) {
+    var number = $("#num");
+    var data = [];
+    if (validation(number.val())) {
+      $("#num").removeClass("has-error");
+      data = pingPong(number.val());
+      display(data);
+    } else {
+      number.addClass("has-error");
+    }
+    event.preventDefault();
+  });
   
 });
